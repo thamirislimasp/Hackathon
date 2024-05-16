1154c589fa63953dfa7aef12d78e99cf94095dff
Yago <142351190+YagoABSC@users.noreply.github.com>
2024-05-16 18:17:39 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Interação Modal-Login-Cadastro
+
+// Get the modal
+var modalLogin = document.getElementById('id01');
+var modalCad = document.getElementById('id02');
+
+function abreFechaCad(){
+    modalLogin.style.display = "none";
+    modalCad.style.display = "block";
+}
+
+function abreFechaLogin(){
+    modalLogin.style.display = "block";
+    modalCad.style.display = "none";
+}
+
+// Interação Agendamento
+
+let ctnAgendar = document.getElementById('agendamento')
+let ctnAlterar = document.getElementById('alterar-agenda')
+let ctnRelatar = document.getElementById('relatorio')
+
+function agendar(){
+    ctnAgendar.style.display = "block"
+    ctnAlterar.style.display = "none"
+    ctnRelatar.style.display = "none"
+}
+
+function alterar(){
+    ctnAgendar.style.display = "none"
+    ctnAlterar.style.display = "block"
+    ctnRelatar.style.display = "none"
+}
+
+function gerar(){
+    ctnAgendar.style.display = "none"
+    ctnAlterar.style.display = "none"
+    ctnRelatar.style.display = "block"
+}
